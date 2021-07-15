fd1644f17cc5dba40d010a688ba0cd1ec3c74943
suhtoshi <haydenk.lewis@gmail.com>
2021-07-15 10:17:11 -0500

    added files for new gatsby blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,60 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
 
-export default IndexPage
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => {
+  console.log(data)
+  return(  
+<Layout>
+    <Seo title="Home" />
+    <div>
+      <h1>Suhtoshi's Thoughts</h1>
+      <h4>{ data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) =>(
+          <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+          </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
+    </div>
+  </Layout>)
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark (sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          fields{
+            slug
+          }
+          html
+          excerpt
+        }
+      }
+    }
+  }
+`
