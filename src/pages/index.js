169790b0c815feee0738f062226176c31b89f6d5
suhtoshi <haydenk.lewis@gmail.com>
2021-07-15 10:38:01 -0500

    edited posts on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default ({ data }) => {
     <Seo title="Home" />
     <div>
       <h1>Suhtoshi's Thoughts</h1>
-      <h4>{ data.allMarkdownRemark.totalCount}Posts</h4>
+      <h4>{ data.allMarkdownRemark.totalCount} Posts</h4>
       {
         data.allMarkdownRemark.edges.map(({node}) =>(
           <div key={node.id}>
