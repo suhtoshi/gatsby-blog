a82329e43d34c2d5a8c951c8d5ad831d2805d7e5
suhtoshi <haydenk.lewis@gmail.com>
2021-07-15 14:08:59 -0500

    added sticky header and fixed typos

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,8 @@ const Header = ({ siteTitle }) => (
     style={{
       background: `rebeccapurple`,
       marginBottom: `1.45rem`,
+      position: `sticky`,
+      top: `0`,
     }}
   >
     <div
